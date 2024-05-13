6992b26f154e5f1f3197a8b8cef1a868bd97eaad
alt--er <ogasawara.t.ae@m.titech.ac.jp>
2024-05-14 01:12:58 +0900

    gitignoreの設定

--- server/init.sql
+++ server/init.sql
@@ -9,19 +9,23 @@ Genre,
 User,
 Restaurant;
 
-CREATE TABLE Area (id CHAR(36) PRIMARY KEY, areaname VARCHAR(255));
+CREATE TABLE
+  Area (id CHAR(36) PRIMARY KEY, areaname VARCHAR(255));
 
-CREATE TABLE Genre (id CHAR(36) PRIMARY KEY, genrename VARCHAR(255));
+CREATE TABLE
+  Genre (id CHAR(36) PRIMARY KEY, genrename VARCHAR(255));
 
-CREATE TABLE User (
-  id CHAR(36) PRIMARY KEY,
-  name VARCHAR(255),
-  traQid VARCHAR(255)
-);
+CREATE TABLE
+  User (
+    id CHAR(36) PRIMARY KEY,
+    name VARCHAR(255),
+    traQid VARCHAR(255)
+  );
 
-CREATE TABLE Restaurant (
-  id CHAR(36) PRIMARY KEY,
-  name VARCHAR(255),
-  areaid CHAR(36),
-  FOREIGN KEY (areaid) REFERENCES Area (id)
-);
+CREATE TABLE
+  Restaurant (
+    id CHAR(36) PRIMARY KEY,
+    name VARCHAR(255),
+    areaid CHAR(36),
+    FOREIGN KEY (areaid) REFERENCES Area (id)
+  );

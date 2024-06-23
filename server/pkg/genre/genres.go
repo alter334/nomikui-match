7d0aafe4c642c85ae506ff0a630d7145fcb9767e
Alter <tuuchan0420@gmail.com>
2024-06-23 16:34:41 +0900

    genre系の骨子

---
+++ server/pkg/genre/genres.go
@@ -0,0 +1,17 @@
+package genre
+
+import (
+	"github.com/labstack/echo/v4"
+)
+
+type Genreservice struct{}
+
+func (s *Genreservice) GetGenres(ctx echo.Context) error {
+
+	return nil
+}
+
+func (s *Genreservice) PostGenres(ctx echo.Context) error {
+	
+	return nil
+}
---
+++ server/pkg/genre/genresgenreid.go
@@ -0,0 +1,17 @@
+package genre
+
+import (
+	"github.com/labstack/echo/v4"
+)
+
+func (s *Genreservice) GetGenresGenreid(ctx echo.Context) error {
+	return nil
+}
+
+func (s *Genreservice) PatchGenresGenreid(ctx echo.Context) error {
+	return nil
+}
+
+func (s *Genreservice) DeleteGenresGenreid(ctx echo.Context) error {
+	return nil
+}

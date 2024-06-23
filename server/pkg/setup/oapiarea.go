6be8af19e6b3dbe38f4f19cdcfd32b5b97c0b99d
Alter <tuuchan0420@gmail.com>
2024-06-23 16:33:36 +0900

    oapi対応用のファイル準備

---
+++ server/pkg/setup/oapiarea.go
@@ -0,0 +1,26 @@
+package setup
+
+import (
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
+)
+
+func (s *OapiService) GetAreas(ctx echo.Context) error {
+	return nil
+}
+
+func (s *OapiService) PostAreas(ctx echo.Context) error {
+	return nil
+}
+
+func (s *OapiService) GetAreasAreaid(ctx echo.Context, areaid uuid.UUID) error {
+	return nil
+}
+
+func (s *OapiService) PatchAreasAreaid(ctx echo.Context, areaid uuid.UUID) error {
+	return nil
+}
+
+func (s *OapiService) DeleteAreasAreaid(ctx echo.Context, areaid uuid.UUID) error {
+	return nil
+}
---
+++ server/pkg/setup/oapigenre.go
@@ -0,0 +1,28 @@
+package setup
+
+import (
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
+)
+
+func (s *OapiService) GetGenres(ctx echo.Context) error {
+
+	return nil
+}
+
+func (s *OapiService) PostGenres(ctx echo.Context) error {
+
+	return nil
+}
+
+func (s *OapiService) GetGenresGenreid(ctx echo.Context, genreid uuid.UUID) error {
+	return nil
+}
+
+func (s *OapiService) PatchGenresGenreid(ctx echo.Context, genreid uuid.UUID) error {
+	return nil
+}
+
+func (s *OapiService) DeleteGenresGenreid(ctx echo.Context, genreid uuid.UUID) error {
+	return nil
+}

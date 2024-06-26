e922071a4986395d715b3e0892ea4ce5006ec14c
Alter <tuuchan0420@gmail.com>
2024-06-26 19:48:09 +0900

    移送用

--- server/pkg/area/areas.go
+++ server/pkg/area/areas.go
@@ -9,8 +9,8 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-func (a *AreaService) GetAreas() (res *oapi.Area, err error) {
-	err = a.db.Select(res, "SELECT * FROM `area`")
+func (a *AreaService) GetAreas() (res []oapi.Area, err error) {
+	err = a.db.Select(&res, "SELECT * FROM `area`")
 	if err != nil {
 		return nil, echo.NewHTTPError(http.StatusInternalServerError, fmt.Sprintf("%+v", err))
 	}
@@ -18,8 +18,9 @@ func (a *AreaService) GetAreas() (res *oapi.Area, err error) {
 }
 
 func (a *AreaService) PostAreas(req *oapi.PostAreasJSONRequestBody) (res *oapi.Area, err error) {
-	req.Id = uuid.New()
-	_, err = a.db.Exec("INSERT INTO `area` VALUES(?,?)", req.Id, req.Areaname)
+	uuid := uuid.New()
+	req.Id = &uuid
+	_, err = a.db.Exec("INSERT INTO `area` VALUES(?,?)", *req.Id, req.Areaname)
 	if err != nil {
 		return nil, echo.NewHTTPError(http.StatusInternalServerError, fmt.Sprintf("%+v", err))
 	}
